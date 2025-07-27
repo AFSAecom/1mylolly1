8fede4fc495ce57c739afacd66b25c69448ee1dd
AFSAecom <afsaecom@gmail.com>
2025-07-27 19:31:22 +0100

    Update seed.sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,8 +1,7 @@
+-- INSERT de test pour utilisateurs
 
--- Exemple d'utilisateur
-INSERT INTO public.users (
-    id, nom, prenom, email, role, code_client, telephone, whatsapp, date_naissance
-) VALUES (
-    'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa',
-    'Doe', 'John', 'john@lolly.tn', 'client', 'C0001', '12345678', '12345678', '1990-01-01'
-);
+INSERT INTO public.users (nom, prenom, email, role, code_client)
+VALUES
+('Admin', 'Principal', 'admin@example.com', 'admin', 'CLT001'),
+('Selim', 'Lima', 'selim@example.com', 'client', 'CLT002'),
+('Nour', 'Ben Salem', 'nour@example.com', 'conseillere', 'CLT003');
