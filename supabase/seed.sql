f362b7e5485cf667fc82392603e1b7440cf5730e
AFSAecom <afsaecom@gmail.com>
2025-07-27 02:53:39 +0100

    Update seed.sql

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,13 +1,14 @@
--- Exemple d'insertion supplémentaire pour tester
+-- SEED.SQL : Insertion des utilisateurs
 
-INSERT INTO users (id, nom, prenom, email, role, code_client)
-VALUES
-  (gen_random_uuid(), 'Test', 'Client', 'test.client@lolly.tn', 'client', 'CLI999');
+insert into users (id, nom, prenom, email, role, code_client)
+values
+    -- Admin
+    ('f371e933-e497-4932-b95e-b4ee72bd9337', 'Admin', 'Lolly', 'admin@lolly.tn', 'admin', 'ADM0001'),
 
-INSERT INTO products (id, nom, description, prix, famille_olfactive)
-VALUES
-  (gen_random_uuid(), 'Test Parfum', 'Description test', 99.999, 'Testé');
+    -- Conseillères
+    ('dc425a02-50ab-46a4-b45d-243068f37f2b', 'Ali', 'Ben Salah', 'ali.conseiller@lolly.tn', 'conseillere', 'CON001'),
+    ('3604d069-480d-4e38-b8c1-aa99226705e6', 'Sana', 'Trabelsi', 'sana.conseiller@lolly.tn', 'conseillere', 'CON002'),
 
-INSERT INTO promotions (id, nom, pourcentage, date_debut, date_fin)
-VALUES
-  (gen_random_uuid(), 'Test Promo', 20, '2025-09-01', '2025-09-30');
+    -- Clients
+    ('39b72f45-621a-4fc1-8847-3647c2218ea3', 'Mouna', 'Gharbi', 'mouna.client@lolly.tn', 'client', 'CLI001'),
+    ('e023acea-50a7-4093-aa36-056e7fa8759d', 'Youssef', 'Ayari', 'youssef.client@lolly.tn', 'client', 'CLI002');
